6288548578352e28e15d7eefc8275e4c2f9691ea
bradserlis <bradserlis@yahoo.com>
2018-06-07 22:48:34 -0700

    styling...

--- wop/planner/static/main.js
+++ wop/planner/static/main.js
@@ -4,10 +4,6 @@ console.log('your javascript file is working')
 $(document).ready(function(){
   M.AutoInit();
 
-  $('.new-activity-button').on('click', function(){
-    var toastHTML = '<span>I am toast content</span><button class="btn-flat toast-action">Undo</button>';
-    M.toast({html: toastHTML});
-})
 
 })
 
