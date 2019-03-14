81dadf3b09a0bbe9c7717856e6754ae0922ba38f
bradserlis <bradserlis@yahoo.com>
2019-03-14 12:30:44 -0700

    Correct Whitenoise middleware url

--- wop/settings.py
+++ wop/settings.py
@@ -43,13 +43,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'http://whitenoise.evans.io/en/stable/django.html',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'http://whitenoise.evans.io/en/stable/django.html',
 ]
 
 ROOT_URLCONF = 'wop.urls'
